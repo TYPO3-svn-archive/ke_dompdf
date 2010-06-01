3d9cc890f74b2df7b0817588843aa771dca233c3
feendrache <feendrache@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-01 12:36:37 +0000

    First commit to Forge
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_dompdf/trunk@33813 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "ke_dompdf".
+#
+# Auto generated 19-05-2010 12:11
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'ke DomPDF',
+	'description' => 'typo3output => pdf',
+	'category' => 'misc',
+	'author' => 'Nadine Schwingler',
+	'author_email' => 'schwingler@kennziffer.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"4de7";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"4f1f";s:19:"doc/wizard_form.dat";s:4:"d83a";s:20:"doc/wizard_form.html";s:4:"7f5f";s:29:"pi1/class.tx_kedompdf_pi1.php";s:4:"dcfb";}',
+);
+
+?>
